7f416a58a8b8617dd780f0abe063884b0ad0d3f1
Matthew Kosarek <mattkae@protonmail.com>
2021-10-28 08:58:19 -0400

    (mkosarek) Build file

---
+++ build.sh
@@ -0,0 +1,7 @@
+cd Lesson1 && npm install && npm run build && cd ..
+cd Lesson2 && npm install && npm run build && cd ..
+cd Lesson3 && npm install && npm run build && cd ..
+cd Lesson4 && npm install && npm run build && cd ..
+cd Lesson5 && npm install && npm run build && cd ..
+cd Lesson6 && npm install && npm run build && cd ..
+cd Lesson7 && npm install && npm run build && cd ..
